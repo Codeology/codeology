77e191c1e05091b35a840c3236599f26ac9b5831
Albert Hu <alberthu@berkeley.edu>
2017-02-23 01:33:58 -0800

    Annotate password into Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string
-#  email      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string
+#  email           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string
 #
 
 class User < ApplicationRecord
