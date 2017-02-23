ad974ce9aa01096dba63dcacdf4bb07be1bfe28d
Albert Hu <alberthu@berkeley.edu>
2017-02-23 01:27:27 -0800

    Add password, validations, and tests to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }
 end
