5c789781449a3f8c0b1c98c41ce512b3b35aeca5
Albert Hu <alberthu@berkeley.edu>
2017-03-11 19:21:31 -0800

    StaticPages: simplify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/team'
+
+  root 'static_pages#home'
+
+  get 'help', :to => "static_pages#help"
+  get 'about', :to => "static_pages#about"
+  get 'team', :to => "static_pages#team"
 
   resources :users
 end
