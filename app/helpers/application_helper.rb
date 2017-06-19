6ca8ffae2631305b32d710e8907ac85c794959ea
edwardqiu <edwardqiu@users.noreply.github.com>
2017-06-19 10:57:20 -0700

    added search bar for navbar when user is logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
             link_to body, url, html_options
         end
     end
+    def link_to_nav_dropdown_item(body, url, html_options = {})
+        active = "active" if current_page?(url)
+        content_tag :li, class: active do
+            link_to body, url, html_options
+        end
+    end
 end
