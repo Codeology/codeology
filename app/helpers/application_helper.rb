27ca9ba0e2971c9c06bdc15131db946e3145df95
edwardqiu <edwardqiu@users.noreply.github.com>
2017-06-19 10:58:06 -0700

    cleaning up some code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,8 @@
 module ApplicationHelper
-    def link_to_nav_item(body, url, html_options = {})
-        active = "active" if current_page?(url)
-        content_tag :li, class: active do
-            link_to body, url, html_options
-        end
+  def link_to_nav_item(body, url, html_options = {})
+    active = "active" if current_page?(url)
+    content_tag :li, class: active do
+      link_to body, url, html_options
     end
-    def link_to_nav_dropdown_item(body, url, html_options = {})
-        active = "active" if current_page?(url)
-        content_tag :li, class: active do
-            link_to body, url, html_options
-        end
-    end
+  end
 end
