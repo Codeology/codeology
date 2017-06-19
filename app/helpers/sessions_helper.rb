ce721b57fcf10eae5078027ee60c57baf6874653
edwardqiu <edwardqiu@users.noreply.github.com>
2017-06-19 12:07:03 -0700

    linked the dynamic pages on the navbar to correct spots. i think i got displaying the user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,10 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
   def user_is_logged_in?
-    !!session[:user_id]
-    end
+    !current_user.nil?
+  end
 end
