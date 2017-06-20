7608a1c4318fdcc8b7ab8b5d1839ab2140c2bf62
Edward Qiu <edwardqiu@berkeley.edu>
2017-06-19 22:00:00 -0700

    fixed recruitment path in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/franklin', to: "static_pages#franklin"
   get '/help', to: "static_pages#help"
   get '/edward', to: "static_pages#edward"
-  get '/edward', to: "static_pages#recruitment"
+  get '/recruitment', to: "static_pages#recruitment"
   get '/about', to: "static_pages#about"
   get '/team', to: "static_pages#team"
   get '/dashboard', to: "dynamic_pages#dashboard"
