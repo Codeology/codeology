d0f3421ecd7df161946ecb1d262c3483af5e0ab3
edwardqiu <edwardqiu@users.noreply.github.com>
2017-06-24 15:31:12 -0700

    send help

--- app/controllers/dynamic_pages_controller.rb
+++
@@ -1,9 +0,0 @@
-class DynamicPagesController < ApplicationController
-  def dashboard
-    render 'dashboard'
-  end
-
-  def profile
-    render 'profile'
-  end
-end
--- test/controllers/dynamic_pages_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DynamicPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
