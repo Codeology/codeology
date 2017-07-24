46a484718b97a67254cfcb8b8f12c89a35c98acf
Albert Hu <alberthu16@gmail.com>
2017-07-24 01:20:55 -0700

    Require users to be logged in to edit their own profiles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_path = current_user.nil? ? login_path : root_path
+    respond_to do |format|
+      format.json { head :forbidden }
+      format.html { redirect_to redirect_path, :alert => exception.message }
+    end
+  end
 end
