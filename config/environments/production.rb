ad96cf39274223620aadee179eccd4291836a987
Albert Hu <alberthu16@gmail.com>
2017-08-13 00:08:24 -0700

    Set default action_mailer host and hide some form inputs during user signup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: "www.codeology.club" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :sendmail
 
