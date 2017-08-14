038e05a2ac981f267f2a218b3295db9100e3f9c2
Albert Hu <alberthu16@gmail.com>
2017-08-14 04:28:13 -0700

    Remove unused links from navbar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,20 +3,7 @@ class StaticPagesController < ApplicationController
     render layout: 'application_fluid'
   end
 
-  def team
-    render 'team'
-  end
-
   def recruitment
     render 'recruitment'
   end
-
-  def franklin
-    render 'franklin'
-  end
-
-  def edward
-    render 'edward'
-  end
-
 end
