a45256e5ffe683279d989f3b45b392a162900cd3
Hao Le <HaoLe@berkeley.edu>
2018-08-18 15:37:04 -0700

    Blurbs added to model controller view data flow

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,10 +4,13 @@ class StaticPagesController < ApplicationController
     array = projlist.choose_three  
     @project_0_name = array[0][:name]
     @project_0_url = array[0][:imgurl]
+    @project_0_blurb = array[0][:blurb]
     @project_1_name = array[1][:name]
     @project_1_url = array[1][:imgurl]
+    @project_1_blurb = array[1][:blurb]
     @project_2_name = array[2][:name]
     @project_2_url = array[2][:imgurl]
+    @project_2_blurb = array[2][:blurb]
     render layout: 'application_fluid'
   end
 
