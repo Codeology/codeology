3ec02cfcbafc70e3b7f5574b68370a531340550f
Hao Le <HaoLe@berkeley.edu>
2018-08-23 15:14:26 -0700

    updated project pics with higher resolution. Dynamic projects rendering on home and projects page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,24 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
     projlist = Projects.new
-    array = projlist.choose_three  
-    @project_0_name = array[0][:name]
-    @project_0_url = array[0][:imgurl]
-    @project_0_blurb = array[0][:blurb]
-    @project_1_name = array[1][:name]
-    @project_1_url = array[1][:imgurl]
-    @project_1_blurb = array[1][:blurb]
-    @project_2_name = array[2][:name]
-    @project_2_url = array[2][:imgurl]
-    @project_2_blurb = array[2][:blurb]
+    array = projlist.choose(3)
+    @projects = array
     render layout: 'application_fluid'
   end
 
   def about_us
-    render layout: 'application_fluid'
-  end
-
-  def projects
     render layout: 'application_fluid'
   end
 
@@ -32,5 +20,12 @@ class StaticPagesController < ApplicationController
 
   def recruitment
     render 'recruitment'
+  end
+
+  def projects
+    projlist = Projects.new
+    array = projlist.choose() # default parameter is all elements
+    @projects = array
+    render layout: 'application_fluid'
   end
 end
--- app/models/Projects.rb
+++ app/models/Projects.rb
@@ -1,25 +1,29 @@
 class Projects
     # HARD CODED project names, blurbs, and image urls
-    def choose_three
+    def choose(amount = 0)
         project_0 = {:name => "Automated Job Search", :imgurl => "projects/project_0.png", 
             :blurb => "Learn about the wonders of web scraping, APIs, and database management."}
         project_1 = {:name => "iOS Game Dev", :imgurl => "projects/project_1.png",
             :blurb => "Project members will learn how to use XCode and SWIFT to develop an iOS game."}
         project_2 = {:name => "Tensorflow Object Detection", :imgurl => "projects/project_2.png",
-            :blurb => "Tensorflow based object detection adapted to track custom objects through live video feed."}
+            :blurb => "Tensorflow based detection adapted to track custom objects through live video."}
         project_3 = {:name => "Receipt Reader", :imgurl => "projects/project_3.png",
             :blurb => "Use Google Vision API and RegEx to parse receipt image and split a bill among friends."}
         project_4 = {:name => "Audio Visualizer", :imgurl => "projects/project_4.png",
             :blurb => "Python based visualizer that reads input from a microphone and reacts to the sound."}
         project_5 = {:name => "Megaman Infinite Runner", :imgurl => "projects/project_5.png",
             :blurb => "Unity-based 2D Endless Runner Platformer using Megaman X sprites."}
-        project_6 = {:name => "Portfolio Design & Development", :imgurl => "projects/project_6.png",
+        project_6 = {:name => "Portfolio Design & Dev", :imgurl => "projects/project_6.png",
             :blurb => "Design sprint and development with HTML/CSS/JS to create a deployable online portfolio for recruitment."}
     
         array = [project_0, project_1, project_2, project_3, project_4, project_5, project_6]
-        seed = rand(7)
-        toReturn = []
-        toReturn.push(array[seed], array[(seed + 3) % 7], array[(seed + 6) % 7])
+        if amount == 3 then
+            seed = rand(7)
+            toReturn = []
+            toReturn.push(array[seed], array[(seed + 3) % 7], array[(seed + 6) % 7])
+        else
+            toReturn = array
+        end
         return toReturn
     end
 end
