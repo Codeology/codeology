72b90a2624d80cb91eb381ffe084792b1ec5f785
Hao Le <HaoLe@berkeley.edu>
2018-09-03 15:17:49 -0700

    changed about_us path to about paath

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,20 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/calendar', to: "static_pages#calendar"
   get '/projects', to: "static_pages#projects"
-  get '/about_us', to: "static_pages#about_us"
+  get '/about', to: "static_pages#about"
   get '/apply', to: "static_pages#apply"
   get '/help', to: "static_pages#help"
   get '/recruitment', to: "static_pages#recruitment"
-  get '/about', to: "static_pages#about"
-  get '/team', to: "static_pages#team"
+
+  #get '/team', to: "static_pages#team"
+
   get '/signup', to: "users#new"
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
   # D3 graph data paths
-  get '/leadership_team', to: 'd3_graphs#leadership_team'
+  # get '/leadership_team', to: 'd3_graphs#leadership_team'
 
   resources :users, :except => [:new] do
     member do
