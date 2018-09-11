1d8dc5468d2213ecc8f727a03b93e0f4c8a5c035
Hao Le <HaoLe@berkeley.edu>
2018-09-10 23:02:19 -0700

    got rid of unnecessary route declaration

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get '/apply', to: 'static_pages#apply'
 
   get '/signup', to: 'users#new'
-  post '/signup',  to: 'users#create'
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
