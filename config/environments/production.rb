30234071cf303f4fc3ec9346f4915b97d23d8dbe
Hao Le <HaoLe@berkeley.edu>
2018-10-04 15:19:45 -0700

    changed host URL for emails to www.codeology.club from the heroku app domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,7 +105,7 @@ Rails.application.configure do
   # Deliver
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'codeologyclub.herokuapp.com'
+  host = 'www.codeology.club'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
