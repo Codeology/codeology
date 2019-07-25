e797a8eb7f4db8d9e73b29c29408816448037bd2
Haotian Ye <32553361+haotiany@users.noreply.github.com>
2019-07-25 13:17:39 -0700

    updated projects

--- app/models/Projects.rb
+++ app/models/Projects.rb
@@ -1,9 +1,9 @@
 class Projects
     # HARD CODED project names, blurbs, and image urls
     def choose(amount = 0)
-        
+
 =begin
-        project_0 = {:name => "Automated Job Search", :imgurl => "projects/project_0.png", 
+        project_0 = {:name => "Automated Job Search", :imgurl => "projects/project_0.png",
             :blurb => "Learn about the wonders of web scraping, APIs, and database management."}
         project_1 = {:name => "iOS Game Dev", :imgurl => "projects/project_1.png",
             :blurb => "Project members will learn how to use XCode and SWIFT to develop an iOS game."}
@@ -18,26 +18,22 @@ class Projects
         project_6 = {:name => "Portfolio Design & Dev", :imgurl => "projects/project_6.png",
             :blurb => "Design sprint and development with HTML/CSS/JS to create a deployable online portfolio for recruitment."}
 =end
-        project_0 = {:name => "Personal Portfolio", :imgurl => "projects/portfolio.jpg", 
-            :blurb => "1 week of design and 3 weeks of development to create a deployable online portfolio for recruitment."}
-        project_1 = {:name => "ML webdev", :imgurl => "projects/machinelearning.png",
-            :blurb => "Build, deploy and interact with classification, feature extraction, and filtering models for clothing items."}
-        project_2 = {:name => "Travel Scheduler", :imgurl => "projects/travel.png",
-            :blurb => "Travel scheduling web app powered by various routing and graph algorithms."}
-        project_3 = {:name => "Crave", :imgurl => "projects/nlp.png",
-            :blurb => "Build an iOS app that utilizes an NLP chatbot to help users narrow down their preferences and discover new food"}
-        project_4 = {:name => "Quant Trading Algorithms", :imgurl => "projects/quantopian.jpg",
-            :blurb => "Build a stock market trading algorithm for US equities and backtest/deploy it on Quantopian against S&P 500"}
-        project_5 = {:name => "Spotify Recommendations", :imgurl => "projects/spotify.jpg",
-            :blurb => "Build models using data from Spotify’s API to get recommendations from any playlist."}
-        project_6 = {:name => "Todo list iOS app", :imgurl => "projects/iphone.jpg",
-            :blurb => "Intro to iOS app development to create a task list app with basic design implementation"}
-        project_7 = {:name => "Blockchain Application", :imgurl => "projects/blockchain.png",
-            :blurb => "Construct a DApp with different frameworks to create an online betting game."}
-        project_8 = {:name => "Full stack Web Dev", :imgurl => "projects/webdev.jpg",
-            :blurb => "Full stack responsive web app for finding buddies to walk home safely with."}
-    
-            array = [project_0, project_1, project_2, project_3, project_4, project_5, project_6, project_7, project_8]
+        project_0 = {:name => "Personal Website", :imgurl => "projects/portfolio.jpg",
+            :blurb => "Build your own cool, personal website from scratch using HTML, CSS, JavaScript, and jQuery!"}
+        project_1 = {:name => "Music Generation", :imgurl => "projects/musicGeneration.jpg",
+            :blurb => "Explore introductory ML and neural network concepts and apply your newfound knowledge to generate your own music!"}
+        project_2 = {:name => "Whatcha Wanna Do", :imgurl => "projects/whatchaWanna.jpg",
+            :blurb => "Bored and not sure what you want to do? This website searches through Eventbrite to create a schedule for you to get out there and explore!"}
+        project_3 = {:name => "Unity Development", :imgurl => "projects/unityDev.jpg",
+            :blurb => "Over the course of this semester,  we will be building a 2D game in Unity and C#. We want to build something that our team members think is fun and engaging - so, we'll be deciding on what the specific genre our game will be with our project team, whether that's something like Pokemon or Zelda. "}
+        project_4 = {:name => "Movie Recommendations", :imgurl => "projects/movieRecs.jpg",
+            :blurb => "Build a movie recommendation engine using filtering algorithms and visualize it on an interactive Tableau dashboard."}
+        project_5 = {:name => "Web Reacts Only", :imgurl => "projects/webReacts.jpg",
+            :blurb => "Learn how to start building basic Web apps with a focus on React and API calls!"}
+        project_6 = {:name => "TBA", :imgurl => "projects/iphone.jpg",
+            :blurb => "TBA"}
+
+            array = [project_0, project_1, project_2, project_3, project_4, project_5, project_6]
         if amount == 3 then
             seed = rand(9)
             toReturn = []
